6330da2616f2708ad41ba90a28baa333ab09e40b
Henrique Massareli <henrique.massareli@gmail.com>
2022-09-22 18:50:01 -0300

    feat: create send message and list pokemon functions

---
+++ functions/getPokemons.js
@@ -0,0 +1,21 @@
+const getPokemons = (message, client) => {
+  const axios = require("axios");
+  const limit = message.body.split(" ")[1];
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+    .then((response) => {
+      console.log(message.from);
+      client.sendMessage(
+        message.from,
+        response.data.results.reduce((acc, pokemon) => {
+          acc += " " + pokemon.name;
+          return acc;
+        }, "")
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = getPokemons;
---
+++ index.js
@@ -0,0 +1,36 @@
+// if (message.body === "last 10 messages") {
+//   client.getChats().then((chats) => {
+//     client.sendMessage(message.from, JSON.stringify(chats));
+//   });
+// }
+
+//client.pupPage.click("#pane-side")
+
+const qrcode = require("qrcode-terminal");
+
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const getPokemons = require("./functions/getPokemons");
+const sendMessages = require("./functions/sendMessages");
+const client = new Client({
+  authStrategy: new LocalAuth({ clientId: "bot-zdg" }),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+  client.on("message", (message) => {
+    if (message.body.toLowerCase().includes("pokemons")) {
+      getPokemons(message, client);
+    }
+    if (message.body.toLowerCase().includes("disparar mensagens")) {
+      sendMessages(message, client);
+    }
+    console.log(message.body);
+    //get a list of 10 pokemons from the pokeapi and send it to the user
+  });
+});
+
+client.initialize();
